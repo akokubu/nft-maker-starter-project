47e2b432a60ed99435146f0ae28f789ff08b07ae
Atsushi Kokubu <akokubu@gmail.com>
2022-07-15 09:44:43 +0900

    upload to github

--- src/components/NftUploader/NftUploader.jsx
+++ src/components/NftUploader/NftUploader.jsx
@@ -1,11 +1,134 @@
 import { Button } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
 import ImageLogo from "./image.svg";
 import "./NftUploader.css";
+import { ethers } from "ethers";
+import Web3Mint from "../../utils/Web3Mint.json";
+import { Web3Storage } from "web3.storage";
 
+const API_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweENFMDlhQjA2OTcwRTA5NzBjRkQxOTQ4RGY1MEM5NDA1N2RCZDBDQTciLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NTc4MTAzNzAyNjUsIm5hbWUiOiJOZnRVcGxvYWQifQ.s2YvTXJ19uqfeSNWjf0OAe92akNZi55cYxaW8ERPgzg";
 const NftUploader = () => {
+  const [currentAccount, setCurrentAccount] = useState("");
+  console.log("currentAccount: ", currentAccount);
+  const [imagePreview, setImagePreview] = useState(undefined);
+
+  const checkIfWalletIsConnected = async () => {
+    const { ethereum } = window;
+    if (!ethereum) {
+      console.log("Make sure you have MetaMask!");
+      return;
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account", account);
+      setCurrentAccount(account);
+    } else {
+      console.log("No authorized account found");
+    }
+  };
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const askContractToMintNft = async (ipfs) => {
+    const CONTRACT_ADDRESS = "0x41464b1e0634871270E93EE88D4d87891AA2fEFd";
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          Web3Mint.abi,
+          signer
+        );
+        console.log("Going to pop wallet now to pay gas...");
+        let nftTxn = await connectedContract.mintIpfsNFT("sample", ipfs);
+        console.log("Mining...please wait.");
+        await nftTxn.wait();
+        console.log(
+          `Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`
+        );
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const renderNotConnectedContainer = () => (
+    <button
+      onClick={connectWallet}
+      className="cta-button connect-wallet-button"
+    >
+      Connect to Wallet
+    </button>
+  );
+
+  const imageToNFT = async (e) => {
+    setImagePreview(undefined);
+
+    const client = new Web3Storage({ token: API_KEY });
+    const image = e.target;
+    console.log("image: ", image.result);
+
+    // FileReaderクラスのインスタンスを取得
+    const reader = new FileReader();
+    // ファイルを読み込み終わったタイミングで実行するイベントハンドラー
+    reader.onload = (e) => {
+      // imagePreviewに読み込み結果（データURL）を代入する
+      // imagePreviewに値を入れると<output>に画像が表示される
+      setImagePreview(e.target?.result);
+    };
+
+    // ファイルを読み込む
+    // 読み込まれたファイルはデータURL形式で受け取れる（上記onload参照）
+    reader.readAsDataURL(e.target?.files[0]);
+
+    const rootCid = await client.put(image.files, {
+      name: "experiment",
+      maxRetries: 3,
+    });
+    const res = await client.get(rootCid);
+    const files = await res.files();
+    for (const file of files) {
+      console.log("file.cid", file.cid);
+      askContractToMintNft(file.cid);
+    }
+  };
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
   return (
     <div className="outerBox">
+      {currentAccount === "" ? (
+        renderNotConnectedContainer()
+      ) : (
+        <p>If you chose image, you can mint your NFT</p>
+      )}
       <div className="title">
         <h2>NFTアップローダー</h2>
         <p>JpegかPngの画像ファイル</p>
@@ -15,13 +138,28 @@ const NftUploader = () => {
           <img src={ImageLogo} alt="imagelogo" />
           <p>ここにドラッグ＆ドロップしてね</p>
         </div>
-        <input className="nftUploadInput" multiple name="imageURL" type="file" accept=".jpg , .jpeg , .png"  />
+        <input
+          className="nftUploadInput"
+          multiple
+          name="imageURL"
+          type="file"
+          accept=".jpg , .jpeg , .png"
+          onChange={imageToNFT}
+        />
       </div>
       <p>または</p>
       <Button variant="contained">
         ファイルを選択
-        <input className="nftUploadInput" type="file" accept=".jpg , .jpeg , .png"/>
+        <input
+          className="nftUploadInput"
+          type="file"
+          accept=".jpg , .jpeg , .png"
+          onChange={imageToNFT}
+        />
       </Button>
+      <div>
+        <img src={imagePreview} alt="preview" className="imagePreview" />
+      </div>
     </div>
   );
 };
